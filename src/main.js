ba8d1be0da3778b1c4642569d1b41019893c9816
Ismail970 <ismail.mo5324@gmail.com>
2024-01-07 17:45:08 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+'use-strict'
+
+const paperBtn = document.querySelector("#paper")
+const scissorsBtn = document.querySelector("#scissors")
+const rockBtn = document.querySelector("#rock")
+
+console.log(rockBtn)
